f34462035d2381fbc946db294e8610a5c8233c08
Tony KHAOV <tony.khaov@gmail.com>
2020-03-18 00:10:49 +0100

    :tada: Launch CoviD app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [countriesList, setCountriesList] = useState([]);
+  const [covidDatas, setCovidDatas] = useState([]);
+  useEffect(() => {
+    async function fetchAPI() {
+      const res = await fetch(
+        "https://pomber.github.io/covid19/timeseries.json"
+      );
+      const data = await res.json();
+      await setCountriesList(Object.keys(data));
+      await setCovidDatas(Object.values(data));
+    }
+
+    fetchAPI();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container-fluid">
+      <header className="my-3" style={{ textAlign: "center" }}>
+        <h1 className="display-4">
+          CoViD-19 <br />
+          Live Figures
+        </h1>
       </header>
+      <main className="container-fluid">
+        <div className="card card-body">
+          <ul className="list-group card-title">
+            {countriesList.map(country => {
+              return (
+                <li
+                  className="list-group-item"
+                  onMouseEnter={() => console.log("entered")}
+                  onMouseLeave={() => console.log("left")}
+                >
+                  {country}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </main>
     </div>
   );
 }
